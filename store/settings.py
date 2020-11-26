54014bb3ca126f64a59296e22312710ecd530ffc
unknown <Mosaab Laboune>
2020-11-26 03:28:47 +0100

    deploy the app

--- store/settings.py
+++ store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+ALLOWED_HOSTS = ['.herokuapp.com','http://127.0.0.1:8000/']
